266d48427d5621f1571177ef769da357a006d34d
abovefallingsun <ss18155651802@outlook.com>
2025-07-31 13:00:02 +0800

    Initialization

---
+++ src/main/java/com/afs/itech/ImaginaryTech.java
@@ -0,0 +1,13 @@
+package com.afs.itech;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.ModContainer;
+
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
+@Mod(Meta.MODID)
+public class ImaginaryTech {
+    public ImaginaryTech(IEventBus bus, ModContainer container) {
+
+    }
+}
---
+++ src/main/java/com/afs/itech/Meta.java
@@ -0,0 +1,5 @@
+package com.afs.itech;
+
+public class Meta {
+    public static final String MODID = "imaginarytech";
+}
